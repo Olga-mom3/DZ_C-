f3acab2e22369eb91e637381b4d6c2c2c712dbfd
Olga <olgakhar@gmail.com>
2022-12-06 00:38:29 +0300

    edit DZ 1 Seminar 1

--- DZ_1_1/Program.cs
+++ DZ_1_1/Program.cs
@@ -7,11 +7,20 @@
 Console.WriteLine ("Введите первое чисо : ");
 string number1 = Console.ReadLine();
 int num1 = Convert.ToInt32(number1);
+
 Console.WriteLine ("Введите второе чисо : ");
 string number2 = Console.ReadLine();
 int num2 = Convert.ToInt32(number2);
 
  if (num1 > num2)
+ {
  Console.WriteLine ($"Максимальное число: {number1}");
+ }
  else
+ {
 Console.WriteLine ($"Максимальное число: {number2}");
+ }
+ if (num1 == num2)
+ {
+Console.WriteLine (" числа равны ");
+ }
