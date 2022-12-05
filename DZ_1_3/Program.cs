bb1be5d907a0dcd5d921c69e91c4900eabea7061
Olga <olgakhar@gmail.com>
2022-12-05 13:54:28 +0300

    DZ 3 Seminar 1

---
+++ DZ_1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Программа, которая на вход принимает число и 
+// на выход выдает является ли число четным ( делится на два без остатка)
+// 4 -> да
+// -3 -> нет 
+// 7 -> нет 
+
+Console.WriteLine ("Введите число");
+string number = Console.ReadLine();
+int num = Convert.ToInt32 (number);
+if (num % 2 == 0)
+Console.WriteLine ("Четное число");
+ else  
+ Console.WriteLine (" Нечетное число");
