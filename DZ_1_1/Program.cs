d9d09080b86053bea71a5cb06a2e7db35ac081dd
Olga <olgakhar@gmail.com>
2022-12-05 12:03:30 +0300

    DZ 1 Seminar 1

---
+++ DZ_1_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Программа, которая принимает на вход два числа и выдает 
+// какое из них больше
+// a=5 ; b=7 -> max = 7
+// a=2 ; b=10 -> max = 10
+// a=-9 ; b=-3 -> max = -3
+
+Console.WriteLine ("Введите первое чисо : ");
+string number1 = Console.ReadLine();
+int num1 = Convert.ToInt32(number1);
+Console.WriteLine ("Введите второе чисо : ");
+string number2 = Console.ReadLine();
+int num2 = Convert.ToInt32(number2);
+
+ if (num1 > num2)
+ Console.WriteLine ($"Максимальное число: {number1}");
+ else
+Console.WriteLine ($"Максимальное число: {number2}");
