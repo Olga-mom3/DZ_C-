0956ce43f59df7e6a6a3f7ea085a9fcee077b2ef
Olga <olgakhar@gmail.com>
2022-12-06 01:08:54 +0300

    DZ 4 Seminar 1

---
+++ DZ_1_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает 
+// все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine(" Введите число :");
+string enterN = Console.ReadLine();
+int num = Convert.ToInt32(enterN);
+
+int temp = 1;
+while (temp <= num)
+{
+    if (temp % 2 == 0)
+    {
+        Console.WriteLine($" Четные числа : {temp}");
+    }
+    temp++;
+}
