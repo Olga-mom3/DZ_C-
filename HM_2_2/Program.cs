5daa0ccbdea50f65afc667a49997f39c9c7b6747
Olga <olgakhar@gmail.com>
2022-12-07 23:02:11 +0300

    Seminar 2 HomeWork 2

---
+++ HM_2_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine(" Введите число :");
+string enterNum = Console.ReadLine();
+int number = enterNum.Length;
+
+if (number < 3)
+{
+    Console.WriteLine($" Число состоит меньше чем из трех цифр");
+}
+ else 
+ { 
+    Console.Write($"третья цифра в числе - ");
+    Console.WriteLine(enterNum.Substring(2, 1));
+ }
