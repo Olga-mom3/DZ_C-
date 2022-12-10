dcc99a21e57a28ec8a769c85925f507cd0fea239
Olga <olgakhar@gmail.com>
2022-12-11 01:54:03 +0300

    Seminar 3 HomWork 1

---
+++ HM_3_1/Program.cs
@@ -0,0 +1,28 @@
+﻿//19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет    12821 -> да 23432 -> да
+
+Console.WriteLine ("Ввести 5 значное число: ");
+string enternum = Console.ReadLine();
+int lenNum = enternum.Length;
+
+if (lenNum == 5)
+{
+    if (enternum[0] == enternum[4] && enternum [1] == enternum[3])
+    {
+        Console.WriteLine("Да, палиндром");
+    }
+    else
+        {
+        Console.WriteLine("Нет, не палиндром");
+        }
+}
+if (lenNum > 5 )
+    { 
+    Console.WriteLine(" число не пятизначное");
+    }
+if (lenNum < 5 )
+   { 
+   Console.WriteLine(" число не пятизначное");
+ }
+
+
