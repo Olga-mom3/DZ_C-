1fe1b7fc85409f00e52f6efc1ba523a1e4198236
Olga <olgakhar@gmail.com>
2022-12-12 00:25:23 +0300

    Seminar 4 HomeWork 2

---
+++ HW_4_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//  27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11    82 -> 10    9012 -> 12
+
+Console.WriteLine(" Введите число : ");
+int enterNum = int.Parse(Console.ReadLine());
+
+int Sum = 0;
+int Num1 = 1;
+int Num2 = enterNum; 
+for (; Num2> 0;)
+{
+    Num1 = Num2 % 10; 
+    Sum = Sum + Num1; 
+    Num2= Num2/ 10;  
+}
+Sum = Sum + Num2; 
+
+Console.WriteLine($" Сумма цифр числа {enterNum} - равна {Sum}");
+
