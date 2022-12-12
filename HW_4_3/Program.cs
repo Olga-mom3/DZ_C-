1dd5bc7e1ce5145b82b737da8457e5ea3b8e3172
Olga <olgakhar@gmail.com>
2022-12-12 11:13:39 +0300

    Seminar 4 HomeWork 3

---
+++ HW_4_3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая  на вход принимает (вводит пользователь)
+//массив из 8 элементов и выводит их на экран.
+
+Console.WriteLine("Введите 8 чисел");
+int[] arr = new int[8]; 
+
+for (int i=0; i<8; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+Console.WriteLine($"[{string.Join(",", arr)}]");
+
+
+/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+
+int[] array = GetRandomArray(8, 0, 100);
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return result;
+}
+Console.WriteLine($"[{String.Join(",", array)}]"); */
+
