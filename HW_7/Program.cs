7ee316aba0a9302915f49434f33f2d56bbba0dd9
Olga <olgakhar@gmail.com>
2022-12-18 00:02:22 +0300

    Seminar 7 HomeWork 47

---
+++ HW_7/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = GetArray(rows, columns, -10, 10);
+
+PrintArray(array);
+Console.WriteLine();
+PrintArray(array);
+
+double[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    double[,] result = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+
+            result[i, j] = Convert.ToDouble(new Random().Next(minValue, maxValue + 1)) + new Random().NextDouble();
+
+        }
+    }
+    return result;
+}
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j].ToString("F" + 2)} "); //ToString("F" + 2))  обрезать хвост
+
+        }
+        Console.WriteLine();
+    }
+}
+Console.ReadLine();
